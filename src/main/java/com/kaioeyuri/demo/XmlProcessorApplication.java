68e0d967750ce8cb659ab2f794781badaf905213
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-18 20:34:02 -0300

    bootstrap spring boot application

---
+++ src/main/java/com/kaioeyuri/demo/XmlProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.kaioeyuri.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaioeyuri/demo/XmlProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaioeyuri.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
