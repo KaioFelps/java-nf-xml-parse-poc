676df410158777cfb5b8c2f867ef5b9f0072ccfc
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-19 18:21:32 -0300

    feat(NationalRegister): add constructor

--- core/src/main/java/com/kaioeyuri/valueObjects/NationalRegister.java
+++ core/src/main/java/com/kaioeyuri/valueObjects/NationalRegister.java
@@ -8,6 +8,11 @@ public class NationalRegister {
     private NationalRegisterType type;
     private String value;
 
+    public NationalRegister(String value, NationalRegisterType type) {
+        this.type = type;
+        this.value = value;
+    }
+
     public Optional<String> getCPF() {
         if (this.type.equals(NationalRegisterType.CPF)) {
             return Optional.of(this.value);
