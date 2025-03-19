b667532f2b0360a282d79b7dbce812b6f0fe0adb
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-19 18:22:35 -0300

    feat: add `NfeXMLTags` class containing static mappings of NF XML keys

---
+++ nfParser/src/main/java/com/kaioeyuri/nfParser/NfeXMLTags.java
@@ -0,0 +1,20 @@
+package com.kaioeyuri.nfParser;
+
+public final class NfeXMLTags {
+    public static final String identitySectionTagName = "ide";
+    public static final String issuerSectionTagName = "emit";
+    public static final String buyerSectionTagName = "dest";
+    public static final String totalSectionTagName = "total";
+    public static final String totalICMSSectionTagName = "icmstot";
+
+    public static final String endOfRelevantDataTag = "infNFe";
+    public static final String productTag = "det";
+    public static final String productInnerTag = "prod";
+
+    public static final String productCodeTag = "cProd";
+    public static final String productNameTag = "xProd";
+    public static final String productCommercialQuantityTag = "qCom";
+    public static final String productUnitaryCostTag = "vUnCom";
+    public static final String productTributaryUnitaryCostTag = "vUnTrib";
+    public static final String productCommercialUnityTag = "uCom";
+}
